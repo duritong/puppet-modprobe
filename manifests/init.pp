1624f74c222588dc05dbc4bc0f701163c3c237d6
Marcel Haerry <haerry@puzzle.ch>
2008-05-13 13:44:14 +0200

    empty module $MODULENAME

---
+++ manifests/init.pp
@@ -0,0 +1,9 @@
+#######################################
+# modprobe module
+# Puzzle ITC - haerry+puppet(at)puzzle.ch
+# GPLv3
+#######################################
+
+
+# modules_dir { "modprobe": }
+class modprobe {}
