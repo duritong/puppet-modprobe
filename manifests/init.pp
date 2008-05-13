bad1c3dcd65eee56534a33f25948c9c976a15264
Marcel Haerry <haerry@puzzle.ch>
2008-05-13 14:02:48 +0200

    added modprobe stuff

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,4 +6,17 @@
 
 
 # modules_dir { "modprobe": }
-class modprobe {}
+class modprobe {
+
+    file{"/etc/modprobe.conf":
+        source => [ "puppet://$server/files/modprobe/${fqdn}/modprobe.conf",
+                    "puppet://$server/files/modprobe/${virtual}/modprobe.conf",
+                    "puppet://$server/files/modprobe/${operatingsystem}/modprobe.conf",
+                    "puppet://$server/files/modprobe/modprobe.conf",
+                    "puppet://$server/modprobe/${virtual}/modprobe.conf",
+                    "puppet://$server/modprobe/${operatingsystem}/modprobe.conf",
+                    "puppet://$server/modprobe/modprobe.conf" ],
+        owner => root, group => 0, mode => 0644;
+    }
+
+}
