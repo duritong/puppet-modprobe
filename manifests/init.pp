5bf3dd9b80eb04285e8aa8ca3d3c464f92fb103d
Simon Josi <sjosi@pw-sjosi.puzzle.itc>
2008-05-29 18:39:42 +0200

    added puzzle copyright notice

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,13 +1,19 @@
-#######################################
+#
 # modprobe module
-# Puzzle ITC - haerry+puppet(at)puzzle.ch
-# GPLv3
-#######################################
-
+#
+# Copyright 2008, Puzzle ITC GmbH
+# Marcel Härry haerry+puppet(at)puzzle.ch
+# Simon Josi josi+puppet(at)puzzle.ch
+#
+# This program is free software; you can redistribute 
+# it and/or modify it under the terms of the GNU 
+# General Public License version 3 as published by 
+# the Free Software Foundation.
+#
 
 # modules_dir { "modprobe": }
-class modprobe {
 
+class modprobe {
     file{"/etc/modprobe.conf":
         source => [ "puppet://$server/files/modprobe/${fqdn}/modprobe.conf",
                     "puppet://$server/files/modprobe/${virtual}/modprobe.conf",
@@ -18,5 +24,4 @@ class modprobe {
                     "puppet://$server/modprobe/modprobe.conf" ],
         owner => root, group => 0, mode => 0644;
     }
-
 }
