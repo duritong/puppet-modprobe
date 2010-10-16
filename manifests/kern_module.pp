958f498b4bd106e07fecfcadcd6be6dad4c30f99
duritong <duritong@immerda.ch>
2010-10-16 17:00:50 +0200

    improving modprobe define

--- manifests/kern_module.pp
+++ manifests/kern_module.pp
@@ -1,47 +1,28 @@
 define modprobe::kern_module(
   $ensure = 'present'
 ){
-  case $operatingsystem {
-    debian,ubuntu: {
-      $modulesfile = '/etc/modules'
-    }
-    default: {
-      $modulesfile = '/etc/rc.modules'
-    }
+  line{"module_${name}":
+    line => $operatingsystem {
+      debian => $name,
+      ubuntu => $name,
+      default => "/sbin/modprobe $name"
+    },
+    file => $operatingsystem {
+      debian => '/etc/modules',
+      ubuntu => '/etc/modules',
+      default => '/etc/rc.modules'
+    },
+    ensure => $ensure,
   }
   case $ensure {
     present: {
-      exec{"insert_module_$name":
-        command => "/bin/echo '/sbin/modprobe $name' >> $modulesfile",
-        unless => "/bin/grep -qFx '/sbin/modprobe $name' $modulesfile",
-      }
       exec{"/sbin/modprobe $name":
         unless => "/bin/grep -q '^$name ' /proc/modules" 
-      }
-      case $operatingsystem {
-        debian,ubuntu: {
-          Exec["insert_module_$name"]{
-            command => "/bin/echo '$name' >> $modulesfile",
-            unless => "/bin/grep -qFx '$name' $modulesfile",
-          }
-        }
       }
     }
     absent: {
       exec{"/sbin/modprobe -r $name":
         onlyif => "/bin/grep -q '^$name ' /proc/modules"
-      }
-      exec{"remove_module_$name":
-        command => "/bin/sed -i '/^\\/sbin\\/modprobe $name$/d' $modulesfile",
-        onlyif => "/bin/grep -qFx '/sbin/modprobe $name' $modulesfile",
-      }
-      case $operatingsystem {
-        debian,ubuntu: {
-          Exec["remove_module_$name"]{
-            command => "/bin/sed -i '/^$name$/d' $modulesfile",
-            onlyif => "/bin/grep -qFx '$name' $modulesfile",
-          }
-        }
       }
     }
     default: {
